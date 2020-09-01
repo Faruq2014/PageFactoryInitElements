c90e97232480fd412b3c2f0adc4e0874f599d04d
Faruq2014 <mjmolla@gmail.com>
2020-09-01 13:04:06 -0400

    sampletest two commit

---
+++ com.salesforce/src/test/java/com/salesforce/logintest/SampaleTest_From1.java
@@ -0,0 +1,11 @@
+package com.salesforce.logintest;
+
+import org.testng.annotations.Test;
+
+public class SampaleTest_From1 {
+	
+@Test
+public void sample1test() {
+	System.out.println("sample1");
+}
+}
